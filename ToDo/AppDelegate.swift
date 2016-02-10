662acea10857c435d2d89c1ecd13623be708fbc9
Jack Bretcher <jack@bretcher.com>
2016-02-09 20:35:19 -0500

    initial build

--- ToDo/AppDelegate.swift
+++ ToDo/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let viewController = ToDoListViewController()
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController=viewController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
---
+++ ToDo/ToDoListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoListViewController.swift
+//  ToDo
+//
+//  Created by Jack Bretcher on 2/9/16.
+//  Copyright © 2016 Sonoma Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor=UIColor.grayColor()
+    }
+
+}
