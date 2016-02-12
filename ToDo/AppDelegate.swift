cfa8cdec4d13c219b8d6c21657eff1c2760671ee
Jack Bretcher <Jack@Bretcher.com>
2016-02-11 21:41:56 -0500

    Added the list controller and navigation

--- ToDo/AppDelegate.swift
+++ ToDo/AppDelegate.swift
@@ -16,11 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
+    
         let viewController = ToDoListViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
         
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        self.window?.rootViewController=viewController
+        self.window?.rootViewController=navigationController
         self.window?.makeKeyAndVisible()
         return true
     }
--- ToDo/ToDoListViewController.swift
+++ ToDo/ToDoListViewController.swift
@@ -8,14 +8,54 @@
 
 import UIKit
 
-class ToDoListViewController: UIViewController {
+class ToDoListViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView?
 
+    var items = NSMutableArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
-        //self.view.backgroundColor=UIColor.grayColor()
+        self.title = "ToDo List"
+        //in the last arg the colon designated there are parameters
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target:  self, action: "didTapAdd:")
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        self.tableView?.dataSource = self
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier:  "CellIdentifier")
+        
+        self.items.addObject("Jack")
+        self.items.addObject("Jill")
     }
-
+    
+    override func setEditing(editing: Bool, animated: Bool) {
+        print("Edit")
+    }
+    //MARK:  UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.items.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier")!
+        
+        let item = self.items[indexPath.row] as! String
+        cell.textLabel?.text = item
+        return cell
+    }
+    
+    
+    //MARK:Actions
+    
+    func didTapAdd(sender: UIBarButtonItem){
+        print ("Add")
+        
+    }
+    
 }
